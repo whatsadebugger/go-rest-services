221c33fcedefb8610df09e37db1ac2dce40a4793
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-07-31 19:47:25 -0500

    query parameters gmux

--- mux/gorilla/gorilla.go
+++ mux/gorilla/gorilla.go
@@ -16,6 +16,14 @@ func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ID: %v\n", vars["id"])
 }
 
+// QueryHandler will return the category and id of an article from query
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category : %v\n", params["category"])
+	fmt.Fprintf(w, "ID: %v\n", params["id"])
+}
+
 func no(err error) {
 	if err != nil {
 		panic(err)
@@ -23,16 +31,21 @@ func no(err error) {
 }
 
 func main() {
-	rtr := mux.NewRouter()
+	r := mux.NewRouter()
 
-	rtr.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler).Name("articleRoute")
+	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler).Name("articleRoute")
+	r.HandleFunc("/articles", QueryHandler).Name("q").Queries("category", "", "id", "")
 
-	url, err := rtr.Get("articleRoute").URL("category", "comedy", "id", "1")
+	url, err := r.Get("articleRoute").URL("category", "comedy", "id", "1")
+	no(err)
+	fmt.Println(url)
+
+	url, err = r.Get("q").URL()
 	no(err)
 	fmt.Println(url)
 
 	server := &http.Server{
-		Handler:      rtr,
+		Handler:      r,
 		Addr:         "localhost:8000",
 		ReadTimeout:  time.Second * 3,
 		WriteTimeout: time.Second * 3,
